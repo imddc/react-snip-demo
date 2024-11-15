a0f96738ebd50bb74900e26a9365c1af39fc580f
imddc <easyhoo2426@163.com>
2024-11-15 18:29:08 +0800

    feat(snip): capture body

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,13 +1,39 @@
 import { createLazyFileRoute } from '@tanstack/react-router'
+import html2canvas from 'html2canvas'
+import { useState } from 'react'
 
 export const Route = createLazyFileRoute('/')({
   component: Index,
 })
 
 function Index() {
+  const [bg, setBg] = useState('')
+
+  // 截图整个body
+  async function captureBody() {
+    const root = document.getElementById('root')
+    if (!root) {
+      return ''
+    }
+    const canvas = await html2canvas(root)
+    const bgDataUrl = canvas.toDataURL('image/png')
+    return bgDataUrl
+  }
+
+  // 开始流程
+  async function startCapture() {
+    const bgDataUrl = await captureBody()
+    setBg(bgDataUrl)
+  }
+
   return (
     <div className="p-2">
-      <h3>Welcome Home!</h3>
+      <h3>
+        Welcome Home!
+        <button type="button" onClick={startCapture}>截图</button>
+      </h3>
+
+      <img alt="" src={bg} />
     </div>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
+import { resolve } from 'node:path'
 import ViteReact from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
   plugins: [
     TanStackRouterVite(),
     ViteReact(),
