78d194e0b9d6f0879acdde2907baaa2af6ba07d5
imddc <easyhoo2426@163.com>
2024-11-15 21:34:36 +0800

    perf(snip): better

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,6 +1,6 @@
 import { createLazyFileRoute } from '@tanstack/react-router'
 import html2canvas from 'html2canvas'
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import ToolBox from '@/components/ToolBox'
 
 export const Route = createLazyFileRoute('/')({
@@ -12,7 +12,7 @@ function Index() {
 
   const [isCapturing, setIsCapturing] = useState(false)
   const [isDrawing, setIsDrawing] = useState(false)
-  const [bgDataUrl, setBgDataUrl] = useState('')
+  const [bgCanvas, setBgCanvas] = useState<HTMLCanvasElement | null>(null)
   const [startPoint, setStartPoint] = useState<{ x: number, y: number } | null>(null)
   const [endPoint, setEndPoint] = useState<{ x: number, y: number } | null>(null)
 
@@ -26,8 +26,7 @@ function Index() {
       return ''
     }
     const canvas = await html2canvas(root)
-    const bgDataUrl = canvas.toDataURL('image/png')
-    setBgDataUrl(bgDataUrl)
+    setBgCanvas(canvas)
   }
 
   function setCanvasBg() {
@@ -35,11 +34,7 @@ function Index() {
     if (!ctx) {
       return
     }
-    const img = new Image()
-    img.onload = () => {
-      ctx.drawImage(img, 0, 0)
-    }
-    img.src = bgDataUrl
+    ctx.drawImage(bgCanvas!, 0, 0)
   }
 
   function setCanvasMask(maskColor?: string) {
@@ -50,24 +45,6 @@ function Index() {
     ctx.globalCompositeOperation = 'source-over'
     ctx.fillStyle = maskColor || 'rgba(0, 0, 0, 0.3)'
     ctx.fillRect(0, 0, canvasRef.current!.width, canvasRef.current!.height)
-  }
-
-  function drawRect(startPoint: { x: number, y: number }, endPoint: { x: number, y: number }) {
-    const ctx = canvasRef.current?.getContext('2d')
-    if (!ctx) {
-      return
-    }
-    // 绘制虚线,内部填充透明色 忽略遮罩层的颜色
-    ctx.globalCompositeOperation = 'destination-out'
-    ctx.strokeStyle = '#fff'
-    ctx.lineWidth = 0.5
-    ctx.lineJoin = 'round'
-    ctx.lineCap = 'round'
-    ctx.setLineDash([10, 5])
-    ctx.fillRect(startPoint.x, startPoint.y, endPoint.x - startPoint.x, endPoint.y - startPoint.y)
-    ctx.strokeRect(startPoint.x, startPoint.y, endPoint.x - startPoint.x, endPoint.y - startPoint.y)
-    ctx.globalCompositeOperation = 'source-over'
-    ctx.stroke()
   }
 
   function clearCanvas() {
@@ -89,7 +66,6 @@ function Index() {
   function mouseUp() {
     setIsDrawing(false)
 
-    // TODO: 显示工具栏
     const bottom = Math.max(endPoint?.y || 0, startPoint?.y || 0)
     const right = Math.max(endPoint?.x || 0, startPoint?.x || 0)
 
@@ -98,6 +74,9 @@ function Index() {
     ) {
       setIsShowToolBox(true)
     }
+    else {
+      handleCancel()
+    }
   }
 
   function mouseMove(e: React.MouseEvent<HTMLCanvasElement>) {
@@ -105,22 +84,41 @@ function Index() {
       return
     }
     setEndPoint({ x: e.clientX, y: e.clientY })
-    draw()
   }
 
-  function draw() {
-    resetCanvas()
-
-    if (!canvasRef.current || !startPoint || !endPoint) {
+  useEffect(() => {
+    const ctx = canvasRef.current?.getContext('2d')
+    if (!ctx) {
       return
     }
-    drawRect(startPoint, endPoint)
-  }
+    ctx.clearRect(0, 0, canvasRef.current!.width, canvasRef.current!.height)
+    ctx.drawImage(bgCanvas!, 0, 0)
+
+    ctx.globalCompositeOperation = 'source-over'
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.3)'
+    ctx.fillRect(0, 0, canvasRef.current!.width, canvasRef.current!.height)
+
+    if (!startPoint || !endPoint) {
+      return
+    }
+
+    // 绘制虚线,内部填充透明色 忽略遮罩层的颜色
+    ctx.globalCompositeOperation = 'destination-out'
+    ctx.strokeStyle = '#fff'
+    ctx.lineWidth = 0.5
+    ctx.lineJoin = 'round'
+    ctx.lineCap = 'round'
+    ctx.setLineDash([10, 5])
+    ctx.fillRect(startPoint.x, startPoint.y, endPoint.x - startPoint.x, endPoint.y - startPoint.y)
+    ctx.strokeRect(startPoint.x, startPoint.y, endPoint.x - startPoint.x, endPoint.y - startPoint.y)
+    ctx.globalCompositeOperation = 'source-over'
+    ctx.stroke()
+  }, [bgCanvas, startPoint, endPoint])
 
   // 开始流程
   async function startCapture() {
     await captureBody()
-    draw()
+    resetCanvas()
     setIsCapturing(true)
   }
 
